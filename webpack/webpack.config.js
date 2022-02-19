1c040d92921ecec69473f78e82ef650d89619b6c
konrad151 <konrad.k151@gmail.com>
2022-02-19 13:24:13 +0100

    Add space

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,7 @@ const commonConfig = require('./webpack.common.js');
 
 module.exports = (envVars) => {
   const { env } = envVars;
-  const envConfig = require(`./webpack.${env}.js`);// eslint-disable-line
+  const envConfig = require(`./webpack.${env}.js`); // eslint-disable-line
   const config = merge(commonConfig, envConfig);
   return config;
 };
